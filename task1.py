0ef55f26632994c9584455452ae6a8b29a16d480
edgecase01 <prathamvarma178@gmail.com>
2025-04-21 21:29:57 +0530

    uploading task1.py, task2.py and README.md

---
+++ task1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Mulitplication: {a * b}")
+print(f"Division: {a / b}")
